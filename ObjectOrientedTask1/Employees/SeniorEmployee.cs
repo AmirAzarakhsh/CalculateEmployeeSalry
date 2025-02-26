8da42aed28cc99a157a39f4f68150d5ef2fc0207
AmirhosseinAzarakhsh <149069544+AmirAzarakhsh@users.noreply.github.com>
2025-02-26 17:28:29 +0330

    Update SeniorEmployee.cs

--- ObjectOrientedTask1/Employees/SeniorEmployee.cs
+++ ObjectOrientedTask1/Employees/SeniorEmployee.cs
@@ -1,24 +1,12 @@
 ﻿namespace ObjectOrientedTask1.Employees
+
+public class SeniorEmployee : Employee
 {
-    public class SeniorEmployee : Employee
+    public SeniorEmployee(string fname, string lname, string nationalId, decimal basesalary) : base(fname, lname, nationalId)
     {
-        public SeniorEmployee(string fname, string lname, string nationalId, int level, decimal baseSalary, 
-            decimal totalHours, decimal extraHours, decimal employeeRatio = 1.2m) : base(fname, lname, nationalId, level, baseSalary, totalHours, extraHours, employeeRatio)
-        {
-            this.BaseSalary = baseSalary;
-            this.Level = level;
-            this.EmployeeRatio = employeeRatio;
-            this.TotalHourInMonth = totalHours;
-            this.ExtraTimePerHours = extraHours;
-        }
-
-
-        internal override decimal CalculateSalary()
-        {
-            decimal salary = BaseSalary * Level * EmployeeRatio * TotalHourInMonth;
-            decimal Extra = BaseSalary * ExtraTimePerHours * Level * EmployeeRatio * 1.2m;
-
-            return salary + Extra;
-        }
+    }
+    internal override decimal CalculateSalary(int level, decimal baseSalary, decimal totalHours, decimal extraHours, decimal employeeRatio = 1.3m)
+    {
+        return base.CalculateSalary(level, baseSalary, totalHours, extraHours, employeeRatio);
     }
 }
