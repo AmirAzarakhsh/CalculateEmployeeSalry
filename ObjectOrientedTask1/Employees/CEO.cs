d7dc361062d00743849c7877a29cec8c727f1137
AmirhosseinAzarakhsh <149069544+AmirAzarakhsh@users.noreply.github.com>
2025-02-26 17:20:56 +0330

    Update CEO.cs

--- ObjectOrientedTask1/Employees/CEO.cs
+++ ObjectOrientedTask1/Employees/CEO.cs
@@ -1,25 +1,15 @@
-﻿namespace ObjectOrientedTask1.Employees
+﻿namespace ObjectOrientedTask1.Employees;
+
+
+ class CEO : Employee
 {
-    class CEO : Employee
+    public CEO(string fname, string lname, string nationalId , decimal baseSalary) : base(fname, lname, nationalId)
     {
-
-        public CEO(string fname, string lname, string nationalId, int level, decimal baseSalary,
-            decimal totalHours, decimal extraHours, decimal employeeRatio = 3)
-       : base(fname, lname, nationalId, level, baseSalary, totalHours, extraHours, employeeRatio)
-        {
-            this.BaseSalary = baseSalary;
-            this.Level = level;
-            this.EmployeeRatio = employeeRatio;
-            this.TotalHourInMonth = totalHours;
-            this.ExtraTimePerHours = extraHours;
-        }
-
-        internal override decimal CalculateSalary()
-        {
-            decimal salary = BaseSalary * Level * EmployeeRatio * TotalHourInMonth;
-            decimal Extra = BaseSalary * ExtraTimePerHours * Level * EmployeeRatio * 1.2m;
-
-            return salary + Extra;
-        }
+    }
+        
+    internal override decimal CalculateSalary(int level, decimal baseSalary, decimal totalHours, decimal extraHours, decimal employeeRatio = 3)
+    {
+        return base.CalculateSalary(level, baseSalary, totalHours, extraHours, employeeRatio);
     }
 }
+
