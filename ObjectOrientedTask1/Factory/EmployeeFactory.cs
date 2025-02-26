aa8be25129b628ce459ac8ba486a4f81e66c18d1
AmirhosseinAzarakhsh <149069544+AmirAzarakhsh@users.noreply.github.com>
2025-02-26 17:31:58 +0330

    Update EmployeeFactory.cs

--- ObjectOrientedTask1/Factory/EmployeeFactory.cs
+++ ObjectOrientedTask1/Factory/EmployeeFactory.cs
@@ -2,34 +2,54 @@
 using ObjectOrientedTask1.Validators;
 using static ObjectOrientedTask1.Employees.Employee;
 
-namespace ObjectOrientedTask1.Factory
+namespace ObjectOrientedTask1.Factory;
+
+
+class EmployeeFactory : IEmployeeFactory
 {
-    class EmployeeFactory : IEmployeeFactory
+    public Employee CreateEmployee(EmployeeType type, string fname, string lname, string nationalId, decimal baseSalary)
     {
+        if (string.IsNullOrWhiteSpace(fname) || string.IsNullOrWhiteSpace(lname))
+            throw new ArgumentException("First or Lastname cannot be null");
 
-        public Employee CreateEmployee(EmployeeType type, string fname, string lname, string nationalId, int level, decimal baseSalary, decimal totalHours, decimal extraHours = 0 )
+        if (!NationalCodeValidator.validate(nationalId))
+            throw new ArgumentException("National Id Isn't Valid");
+
+        BaseSalaryValidator.BaseSalaryValid(baseSalary, type);
+
+        return type switch
         {
-            if (string.IsNullOrWhiteSpace(fname) || string.IsNullOrWhiteSpace(fname))
-                throw new ArgumentException("First or Lastname cannot be null");
-
-            if (!NationalCodeValidator.validate(nationalId))
-                throw new ArgumentException("National Code isn't valid ,\nPlease enter correct national code");
-
-            BaseSalaryValidator.BaseSalaryValid(baseSalary, type);
-                
-            
-            
-            return type switch
+            EmployeeType.SimpleEmployee => new SimpleEmployee(fname, lname, nationalId, baseSalary)
             {
-                EmployeeType.SimpleEmployee => new SimpleEmployee(fname, lname, nationalId, level, baseSalary, totalHours, extraHours),
-                EmployeeType.SeniorEmployee => new SeniorEmployee(fname, lname, nationalId, level, baseSalary, totalHours, extraHours),
-                EmployeeType.Deputy => new SectionManager(fname, lname, nationalId, level, baseSalary, totalHours, extraHours),
-                EmployeeType.DepartmentManager => new DepartmentManager(fname, lname, nationalId, level, baseSalary, totalHours, extraHours),
-                EmployeeType.SectionManager => new SectionManager(fname, lname, nationalId, level, baseSalary, totalHours, extraHours),
-                EmployeeType.CEO => new CEO(fname, lname, nationalId, level, baseSalary, totalHours, extraHours),
-
-                _ => throw new ArgumentException("type of Employee is invalid.")
-            };
-        }
+                FullName = $"{fname} {lname}",
+                NationalCode = nationalId,
+            },
+            EmployeeType.SeniorEmployee => new SeniorEmployee(fname,lname, nationalId, baseSalary)
+            {
+                FullName = $"{fname} {lname}",
+                NationalCode = nationalId
+            },
+            EmployeeType.Deputy => new Deputy(fname, lname, nationalId, baseSalary)
+            {
+                FullName = $"{fname} {lname}",
+                NationalCode = nationalId
+            },
+            EmployeeType.DepartmentManager => new DepartmentManager(fname, lname, nationalId, baseSalary)
+            {
+                FullName = $"{fname} {lname}",
+                NationalCode = nationalId
+            },
+            EmployeeType.SectionManager => new SectionManager(fname, lname, nationalId, baseSalary)
+            {
+                FullName = $"{fname} {lname}",
+                NationalCode = nationalId
+            },
+            EmployeeType.CEO => new CEO(fname, lname, nationalId, baseSalary)
+            {
+                FullName = $"{fname} {lname}",
+                NationalCode = nationalId
+            },
+            _ => throw new ArgumentException("type of Employee is invalid.")
+        };
     }
 }
