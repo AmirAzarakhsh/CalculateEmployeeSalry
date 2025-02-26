d32b557d63bb39cbd24076a7e8f39797376cf568
AmirhosseinAzarakhsh <149069544+AmirAzarakhsh@users.noreply.github.com>
2025-02-26 17:18:13 +0330

    Update Employee.cs

--- ObjectOrientedTask1/Employees/Employee.cs
+++ ObjectOrientedTask1/Employees/Employee.cs
@@ -2,40 +2,46 @@
 namespace ObjectOrientedTask1.Employees
 {
     public abstract class Employee
+{
+    protected Employee(string fname
+       , string lname
+       , string nationalId
+       )
     {
-        protected Employee(string fname
-            , string lname
-            , string nationalId
-            , int level, decimal baseSalary, decimal totalHours, decimal extraHours, decimal employeeRatio)
-        {
-            FullName = $"{fname} {lname}";
-            NationalCode = nationalId;
-            Level = level;
-            BaseSalary = baseSalary;
-            TotalHourInMonth = totalHours;
-            ExtraTimePerHours = extraHours;
-            EmployeeRatio = employeeRatio;
-        }
+        FullName = $"{fname} {lname}";
+
+        NationalCode = nationalId;
+        
+    }
 
 
-        public string FullName { get; }
-        public  string NationalCode { get; init; }
-        public decimal ExtraTimePerHours { get; set; }
-        public decimal TotalHourInMonth { get; set; }
-        public decimal BaseSalary { get; set; }
-        public int Level { get; set; }
-        protected decimal EmployeeRatio { get; set; }
+    public string FullName { get; set; }
+    public required string NationalCode { get; set; }
+    public decimal ExtraTimePerHours { get; set; }
+    public decimal TotalHourInMonth { get; set; }
+    public decimal BaseSalary { get; set; }
+    public int Level { get; set; }
+    protected decimal EmployeeRatio { get; set; }
 
-        public enum EmployeeType
-        {
-            SimpleEmployee,
-            SeniorEmployee,
-            Deputy,
-            DepartmentManager,
-            CEO,
-            SectionManager
-        }
+    public enum EmployeeType
+    {
+        SimpleEmployee,
+        SeniorEmployee,
+        Deputy,
+        DepartmentManager,
+        CEO,
+        SectionManager
+    }
 
-        internal abstract decimal CalculateSalary();
+    internal virtual decimal CalculateSalary(int level
+        , decimal baseSalary, decimal totalHours, decimal extraHours, decimal employeeRatio = 1)
+    {
+        decimal salary = baseSalary * (1 + level / 10) * employeeRatio * totalHours;
+        decimal Extra = baseSalary * extraHours * (1 + level / 10) * employeeRatio * 1.2m;
+        return salary + Extra;
+    }
+    public override string ToString()
+    {
+        return $"FullName : {FullName}\nNationalId : {NationalCode}";
     }
 }
