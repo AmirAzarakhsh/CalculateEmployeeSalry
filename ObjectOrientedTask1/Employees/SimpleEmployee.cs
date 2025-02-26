a16db4104cfd47c5d7c9dabfa9a698710365dcfa
AmirhosseinAzarakhsh <149069544+AmirAzarakhsh@users.noreply.github.com>
2025-02-26 17:29:16 +0330

    Update SimpleEmployee.cs

--- ObjectOrientedTask1/Employees/SimpleEmployee.cs
+++ ObjectOrientedTask1/Employees/SimpleEmployee.cs
@@ -1,24 +1,9 @@
-﻿namespace ObjectOrientedTask1.Employees
+﻿namespace ObjectOrientedTask1.Employees;
+
+public SimpleEmployee(string fname, string lname, string nationalId,decimal basesalary) : base(fname, lname, nationalId)
 {
-    public class SimpleEmployee : Employee
-    {
-        public SimpleEmployee(string fname, string lname, string nationalId, int level, decimal baseSalary,
-            decimal totalHours, decimal extraHours, decimal employeeRatio = 1)
-        : base(fname, lname, nationalId, level, baseSalary, totalHours, extraHours, employeeRatio) {
-            this.BaseSalary = baseSalary;
-            this.Level = level;
-            this.EmployeeRatio = employeeRatio;
-            this.TotalHourInMonth = totalHours;
-            this.ExtraTimePerHours = extraHours;
-        }
-
-        internal override decimal CalculateSalary()
-        {
-
-            decimal salary = BaseSalary * Level * EmployeeRatio * TotalHourInMonth;
-            decimal Extra = BaseSalary * ExtraTimePerHours * Level * EmployeeRatio * 1.2m;
-
-            return salary + Extra;
-        }
-    }
+}
+internal override decimal CalculateSalary(int level, decimal baseSalary, decimal totalHours, decimal extraHours, decimal employeeRatio = 1)
+{
+    return base.CalculateSalary(level, baseSalary, totalHours, extraHours, employeeRatio );
 }
